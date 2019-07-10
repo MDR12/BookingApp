d798a8bc4801929f7fbe3a3912ef8e3f2d6ef263
waritsara.s <waritsara.s@kbtg.tech>
2019-07-10 17:17:03 +0700

    ?

--- app/src/main/java/com/example/meetingroombookingapp/selectbytime/SelectDateTimeActivity.kt
+++ app/src/main/java/com/example/meetingroombookingapp/selectbytime/SelectDateTimeActivity.kt
@@ -7,14 +7,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meetingroombookingapp.R
+import com.example.meetingroombookingapp.common.Constant
 import com.example.meetingroombookingapp.selectmeetingroom.SelectRoomActivity
 import kotlinx.android.synthetic.main.activity_select_date_time.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SelectDateTimeActivity : AppCompatActivity(){
-
-    private val PREFNAME = "MyPreferences"
 
     @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +24,7 @@ class SelectDateTimeActivity : AppCompatActivity(){
         val hour = c.get(Calendar.HOUR)
         val minute = c.get(Calendar.MINUTE)
 
-        val sdf = SimpleDateFormat("dd-MM-yyyy")
+        val sdf = SimpleDateFormat(Constant.FORMAT_DATE)
         var date= sdf.format(Date())
 
         lateinit var start: String
@@ -33,7 +32,7 @@ class SelectDateTimeActivity : AppCompatActivity(){
 
 
         calendarView3.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            date = "$dayOfMonth-"+(month + 1)+"-$year"
+            date = dayOfMonth.toString() + Constant.TEXT_DATH + (month + 1).toString() +  Constant.TEXT_DATH + year.toString()
         }
 
         new_pick_start.setOnClickListener {
@@ -43,7 +42,7 @@ class SelectDateTimeActivity : AppCompatActivity(){
                 c.set(Calendar.HOUR_OF_DAY, h)
                 c.set(Calendar.MINUTE, m)
                 c.set(Calendar.SECOND,0)
-                start = SimpleDateFormat("HH:mm").format(c.time)
+                start = SimpleDateFormat(Constant.FORNAT_TIME).format(c.time)
                 new_pick_start.text = start
 
             }), hour, minute, true)
@@ -58,7 +57,7 @@ class SelectDateTimeActivity : AppCompatActivity(){
                 c.set(Calendar.HOUR_OF_DAY, h)
                 c.set(Calendar.MINUTE, m)
                 c.set(Calendar.SECOND,0)
-                end = SimpleDateFormat("HH:mm").format(c.time)
+                end = SimpleDateFormat(Constant.FORNAT_TIME).format(c.time)
                 new_pick_end.text = end
 
             }), hour + 1, minute, true)
@@ -68,7 +67,7 @@ class SelectDateTimeActivity : AppCompatActivity(){
 
         bt_find_room.setOnClickListener {
 
-            val sp = getSharedPreferences(PREFNAME , Context.MODE_PRIVATE)
+            val sp = getSharedPreferences(Constant.PREF_NAME , Context.MODE_PRIVATE)
             val editor = sp.edit()
 
             editor.putString("date_pick", date)
